3ac579e864747706c8914e7e36d9d502bc01a701
orjwan98 <orjwan98@hotmail.com>
2019-01-05 07:23:24 +0200

    minor bugs

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,15 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const routes = require("../routes/index.js");
 require("env2")("config.env");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "..", "..", "public")));
 app.use(routes);
 app.set("views", path.join(__dirname, "..", "views"));
 app.set("view engine", "hbs");
