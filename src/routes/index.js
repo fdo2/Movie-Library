98f7aba25d8379e2f35658e8a81a33700cf06a86
orjwan98 <orjwan98@hotmail.com>
2019-01-05 06:32:57 +0200

    Make signUp's HTML template #12

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const getPopMovies = require("./getPopMovies.js");
+const register = require("./register");
 
 router.get("/", getPopMovies.get);
-
+router.post("/register", register.post);
 module.exports = router;
